70f4f9579ac8d96b08c76f6a7a8e4929c8f3a8dc
Esteban Garriga <estebangarriga@hotmail.com>
2023-02-16 23:33:32 -0300

    Primer Commit

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const { crearArchivo } = require('./helpers/multiplicar');
 const argv = require('./config/yargs');
+//const argv = require('yargs').argv;
+
 const colors = require('colors');
 
 console.clear();
+
+//const [ , ,arg3 = 'base=5'] = proccess.argv;
+//const[ , base= 5] = arg3.split('=');
+// const base = 5;
+
+//createArchivo( base )
+    //.then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+    //.catch( err => console.log( err ) );
 
 crearArchivo( argv.b, argv.l, argv.h)
     .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'.rainbow) )
